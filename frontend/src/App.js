c210e0ab5c15baf06102d09faa4d3351b282c7de
Oleksandra Oksiuta <alex.oksyuta@gmail.com>
2019-03-02 16:54:38 +0200

    Add css file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import Button from '@material-ui/core/Button';
-import './App.css';
+
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <Button variant="contained">Button</Button>
+                <Button  variant="contained">Button</Button>
             </div>
         );
     }
